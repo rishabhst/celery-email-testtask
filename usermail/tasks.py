beb9c94f56822df15a2a23ae13d9d36ad79f8092
Rishabhstpl <rishabhstpl@gmail.com>
2020-04-13 20:56:14 +0530

    feat: Added functionality to send bulk emails async

--- usermail/tasks.py
+++ usermail/tasks.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from usermail.models import UserProfile
+from usermail.utils.email_helper import send_mail_using_post_office
 
 
 @shared_task
 def send_email_to_all():
-    user_mails = UserProfile.objects.all().values_list('email', flat=True)
-    print(user_emails)
-    print("-------------------------------------")
+    user_emails = UserProfile.objects.all().values_list('email', flat=True)
+    for email in user_emails:
+        send_mail_using_post_office(email, 'Hello There!', 'How are you!')
--- usermail/utils/email_helper.py
+++ usermail/utils/email_helper.py
@@ -1,12 +1,13 @@
 from post_office import mail
 
-def send_mail_using_post_office(from_user, to_user, subject, message):
-	to_user = [to_user]
-	mail.send(
-	    to_user, # List of email addresses also accepted
-	    'rishabhstpl@gmail.com',
-	    subject='My email',
-	    message='Hi there!',
-	    html_message='Hi <strong>there</strong>!',
-	    priority='now',
-	)
+
+def send_mail_using_post_office(to_user, subject, message, from_user='syskart@yandex.com'):
+    to_user = [to_user]
+    mail.send(
+        to_user,  # List of email addresses also accepted
+        from_user,
+        subject=subject,
+        message=message,
+        html_message='Hi <strong>there</strong>!',
+        priority='now',
+    )
